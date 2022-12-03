f55fdea957f7a2e5a95fc11ecbe2935ab534b260
leungyukshing <jacky14.liang@gmail.com>
2022-12-03 00:03:51 -0500

    fix: print out reply in string

--- WFQ/destination.py
+++ WFQ/destination.py
@@ -16,7 +16,7 @@ while True:
 		d = s.recvfrom(1024)
 		reply = d[0]
 		addr = d[1]
-		print('Server reply : ' + reply)
+		print('Server reply : ' + str(reply))
 	except socket.error as msg:
 		print('Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
 		sys.exit()
